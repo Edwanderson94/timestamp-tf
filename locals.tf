27fc5e405db8303dfe58efafb2486dfaea3574c3
Edwanderson Pereira <80295393+Edwanderson94@users.noreply.github.com>
2023-02-13 23:14:18 -0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,14 @@
 locals {
+  #Timestamp
   now = timestamp()
+  
+  #Timezone
   brasilia_tz = timeadd(local.now, "-3h")
   amsterdam_tz = timeadd(local.now, "2h")
+  canada_tz = timeadd(local.now, "-5h")
+  
+  #Format Date
+  date_cad = formatdate("MMM-DD-YYYY") 
   date_br = formatdate("DD/MM/YYYY", local.brasilia_tz)
   date_nl = formatdate("D-MM-YYYY", local.amsterdam_tz)
   date_utc = formatdate("YYYY-MM-DD", local.now)
